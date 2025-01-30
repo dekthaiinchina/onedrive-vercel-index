f64b026c76b7df072b9ae3410285275674c45ea6
在中国的泰国青年_ <132539847+dekthaiinchina@users.noreply.github.com>
2025-01-30 17:50:44 +0700

    Revert "Potential fix for code scanning alert no. 1: Use of password hash with insufficient computational effort"

--- src/utils/protectedRouteHandler.ts
+++ src/utils/protectedRouteHandler.ts
@@ -1,13 +1,12 @@
-import bcrypt from 'bcrypt'
+import sha256 from 'crypto-js/sha256'
 import siteConfig from '../../config/site.config'
 
-// Hash password token with bcrypt
+// Hash password token with SHA256
 function encryptToken(token: string): string {
-  const saltRounds = 10;
-  return bcrypt.hashSync(token, saltRounds);
+  return sha256(token).toString()
 }
 
-// Fetch stored token from localStorage and encrypt with bcrypt
+// Fetch stored token from localStorage and encrypt with SHA256
 export function getStoredToken(path: string): string | null {
   const storedToken =
     typeof window !== 'undefined' ? JSON.parse(localStorage.getItem(matchProtectedRoute(path)) as string) : ''
@@ -16,7 +15,7 @@ export function getStoredToken(path: string): string | null {
 
 /**
  * Compares the hash of .password and od-protected-token header
- * @param odTokenHeader od-protected-token header (bcrypt hashed token)
+ * @param odTokenHeader od-protected-token header (sha256 hashed token)
  * @param dotPassword non-hashed .password file
  * @returns whether the two hashes are the same
  */
@@ -27,7 +26,7 @@ export function compareHashedToken({
   odTokenHeader: string
   dotPassword: string
 }): boolean {
-  return bcrypt.compareSync(dotPassword.trim(), odTokenHeader);
+  return encryptToken(dotPassword.trim()) === odTokenHeader
 }
 /**
  * Match the specified route against a list of predefined routes
